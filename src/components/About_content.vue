80310ff12b88ab62705274c19b39366cc0f72a76
jake <theronbogan1@gmail.com>
2021-02-22 11:45:08 -0500

    update 1.7

--- src/components/About_content.vue
+++ src/components/About_content.vue
@@ -3,16 +3,15 @@
         <div class="about_container">
             <div class="content_container">
                 <h1>About</h1>
-                <img class="user_pic" src="../assets/images/svg/fox_user.svg" alt="Fox Logo">
+                <img class="user_pic" src="../assets/images/svg/about_logo.svg" alt="Fox Logo">
                 <h6>Jake Bogan</h6>
                 <div class="line_break"></div>
                 <p class="about_me_text">
-                    My name is Theron Bogan; however, I like to go by my middle name Jake. As you can tell I like to design and create web applications on the web. I am an inspiring developer and I hope to be a full stack developer soon. I am currently in school for Information Technology and will be soon graduating no later than April of this year. Contact me if you are looking for a better design. 
+                    Hello, my name is Jake and I am a UI/UX designer & your new best friend. I build captivating web experiences that deliver. I am currently in school for Information Technology and will be soon graduating no later than April of this year. If your looking for a better design, 
                     <router-link :to="{name:'Contact'}">
                         <span>Contact</span>
                     </router-link>
-                    me if
-                    your looking for a better design.
+                    me.
                 </p>
             </div>
         </div>
